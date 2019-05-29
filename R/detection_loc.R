a83d383bc2b3791df7e5d3c4d8ee722af04e4fa3
Adam Smith <avianmigration@gmail.com>
2019-05-29 15:11:30 -0400

    Update

--- R/detection_loc.R
+++ R/detection_loc.R
@@ -1,5 +1,7 @@
 # Project location of detection for receivers, by antenna, 
-detection_loc <- function(lat, lon, bearing, distkm) {
+detection_loc <- function(lat, lon, bearing, elems = 9, distkm = NULL) {
+  if (is.null(distkm))
+    distkm <- elems/3
   out <- maptools::gcDestination(lon, lat, bearing, distkm, "km", "WGS84", TRUE)
   colnames(out) = c("det_lon", "det_lat")
   rownames(out) <- NULL
