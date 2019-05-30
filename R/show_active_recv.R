1099c52cf1383a6a155f38f6eb41ddf801dcdf41
Adam Smith <avianmigration@gmail.com>
2019-05-30 19:01:54 -0400

    Daily update; includes spatial detection time

---
+++ R/show_active_recv.R
@@ -0,0 +1,18 @@
+show_active_recv <- function(base = 0, scale = 5, id = "id", spp = "HUGO", NA_inactive = FALSE) {
+  out <- mutate(dly_recv_log,
+                # Add relative, but generic "Y-axis" values for use in adding to plots below
+                y = case_when(
+                  recvSiteName == "E. Pepita"     ~ base + 3 * scale,
+                  recvSiteName == "E. MiraMar"    ~ base + 2 * scale,
+                  recvSiteName == "E. El Pantano" ~ base + 1 * scale,
+                  TRUE                            ~ base),
+                id = recvSiteName)
+  if (!NA_inactive) 
+    out <- filter(out, active)
+  else
+    out <- mutate(out, y = ifelse(active, y, NA))
+  out <- reshape::expand.grid.df(out, data.frame(spp = spp))
+  names(out)[5] <- id
+  out
+}
+  
