f19e78149b0d000f4c455a9283b3b1afd4e9a746
Adam Smith <avianmigration@gmail.com>
2019-06-09 14:47:46 -0400

    Updates

--- R/detection_loc.R
+++ R/detection_loc.R
@@ -7,3 +7,25 @@ detection_loc <- function(lat, lon, bearing, elems = 9, distkm = NULL) {
   rownames(out) <- NULL
   return(as.data.frame(out))
 } 
+
+# Create ellipse geometry in list-column 
+# For use as rowwise mutate
+make_ellipse <- function(x, y, brng, len_km = 2, wid_km = 1.6, crs = 32719) {
+  # Put axes lengths in km
+  maj <- len_km * 1000 / 2
+  min <- wid_km * 1000 / 2
+  # Convert bearing to radians
+  phi <- pi/180 * (90 - brng)
+  # Find ellipse center
+  xc <- x + cos(phi) * maj
+  yc <- y + sin(phi) * maj
+  # Points on a circle
+  t <- seq(0, 2 * pi, length.out = 500)
+  # Create ellipse
+  xi <- xc + maj*cos(t)*cos(phi) - min*sin(t)*sin(phi)
+  yi <- yc + maj*cos(t)*sin(phi) + min*sin(t)*cos(phi)
+  xi <- c(xi, xi[1])
+  yi <- c(yi, yi[1])
+  out <- st_polygon(list(cbind(xi,yi)))
+  st_sfc(out, crs = crs)
+}
--- R/geo_helpers.R
+++ R/geo_helpers.R
@@ -14,3 +14,13 @@ ggmap_rast <- function(map){
   values(blue) <- rgb_cols[['blue']]
   stack(blue,green,red)
 }
+
+sfc_as_cols <- function(x, names = c("x","y")) {
+  stopifnot(inherits(x,"sf") && inherits(sf::st_geometry(x),"sfc_POINT"))
+  ret <- sf::st_coordinates(x)
+  ret <- tibble::as_tibble(ret)
+  stopifnot(length(names) == ncol(ret))
+  x <- x[ , !names(x) %in% names]
+  ret <- setNames(ret,names)
+  dplyr::bind_cols(x,ret)
+}
